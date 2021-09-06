5a87f3014a9167e43b9113dc17540cc254a75e23
55Honey <71938210+55Honey@users.noreply.github.com>
2021-09-06 23:15:00 +0200

    feat(lua): zonewide debuffs
    initial commit

---
+++ zoneDebuff.lua
@@ -0,0 +1,90 @@
+--
+--
+-- Created by IntelliJ IDEA.
+-- User: Silvia
+-- Date: 06/09/2021
+-- Time: 20:17
+-- To change this template use File | Settings | File Templates.
+-- Originally created by Honey for Azerothcore
+-- requires ElunaLua module
+
+
+-- This module debuffs players when they enter or login a zone specified in Config_Zones
+------------------------------------------------------------------------------------------------
+-- ADMIN GUIDE:  -  compile the core with ElunaLua module
+--               -  adjust config in this file
+--               -  add this script to ../lua_scripts/
+------------------------------------------------------------------------------------------------
+-- GM GUIDE:     -  nothing to do. Just watch them die.
+------------------------------------------------------------------------------------------------
+local Config = {}
+
+Config.baseStatModifier = -20
+Config.meleeAPModifier = -20
+Config.rangedAPModifier = -20
+Config.DamageTaken = 20
+Config.DamageDone = -50
+
+local Config_Zones = {}     --zones where to debuff players
+
+table.insert(Config_Zones, 1583) -- Blackrock Spire
+
+local PLAYER_EVENT_ON_LOGIN = 3               -- (event, player)
+local PLAYER_EVENT_ON_UPDATE_ZONE = 27        -- (event, player, newZone, newArea)
+local PLAYER_EVENT_ON_RESURRECT = 36          -- (event, player)
+
+local function has_value(tab, val)
+    for index, value in ipairs(tab) do
+        if value == val then
+            return true
+        end
+    end
+    return false
+end
+
+local function zd_shouldDebuff(player)
+    local zone = player:GetZoneId()
+    if has_value(Config_Zones, zone) then
+        return true
+    end
+
+    return false
+end
+
+local function zd_debuff(player)
+    if not player:HasAura(63388) then
+        player:CastCustomSpell(player, 63388, false, Config.baseStatModifier,Config.meleeAPModifier,Config.rangedAPModifier)
+    end
+    if not player:HasAura(72341) then
+        player:CastCustomSpell(player, 72341, false, Config.DamageTaken,Config.DamageDone)
+    end
+end
+
+local function zd_removeDebuff(player)
+    player:RemoveAura(63388)
+    player:RemoveAura(72341)
+end
+
+local function zd_checkPlayerZone(player)
+    if zd_shouldDebuff(player) then
+        zd_debuff(player)
+    else
+        zd_removeDebuff(player)
+    end
+end
+
+local function zd_checkZoneLogin(event, player)
+    zd_checkPlayerZone(player)
+end
+
+local function zd_checkZoneUpdate(event, player, newZone, newArea)
+    zd_checkPlayerZone(player)
+end
+
+local function zd_checkZoneResurrect(event, player)
+    zd_checkPlayerZone(player)
+end
+
+RegisterPlayerEvent(PLAYER_EVENT_ON_LOGIN, zd_checkZoneLogin)
+RegisterPlayerEvent(PLAYER_EVENT_ON_UPDATE_ZONE, zd_checkZoneUpdate)
+RegisterPlayerEvent(PLAYER_EVENT_ON_RESURRECT,zd_checkZoneResurrect)
