0bf53b3349af1c459d37181576c53f3e101283fd
55Honey <71938210+55Honey@users.noreply.github.com>
2021-10-18 23:33:00 +0200

    feat: add configurable world buff removal

--- zoneDebuff.lua
+++ zoneDebuff.lua
@@ -24,12 +24,14 @@ local ConfigPvP = {}
 local Config_RaidZones = {}        --zones where to debuff players always for PvE
 local Config_DungeonZones = {}     --zones where to debuff players when no rdf
 local Config_PvPZones = {}         --zones where to debuff players always for PvP
+local Config_NoWorldBuffMaps = {}  --maps where to remove world buffs
+local Config_WorldBuff = {}        --spell IDs of world buffs to be removed
 
 -- on/off switch (0/1)
-
 Config.RaidActive = 1
 Config.DungeonActive = 0
 Config.PvPActive = 1
+Config.NoWorldBuffMaps = 1
 
 
 -- all modifiers are in %
@@ -65,6 +67,17 @@ table.insert(Config_PvPZones, 3698) -- Ring of Trials
 table.insert(Config_PvPZones, 3702) -- Blade's Edge Arena
 table.insert(Config_PvPZones, 4378) -- Dalaran Arena
 
+table.insert(Config_NoWorldBuffMaps, 409) -- Molten Core
+table.insert(Config_NoWorldBuffMaps, 469) -- Blackwing Lair
+table.insert(Config_NoWorldBuffMaps, 509) -- Ruins of Ahn'Qiraj
+table.insert(Config_NoWorldBuffMaps, 531) -- Temple of Ahn'Qiraj
+table.insert(Config_NoWorldBuffMaps, 309) -- Zul Gurub
+
+table.insert(Config_WorldBuff, 16609) -- Warchiefs Blessing
+table.insert(Config_WorldBuff, 22888) -- Rallying Cry of the Dragonslayer
+table.insert(Config_WorldBuff, 24425) -- Spirit of Zandalar
+
+
 ------------------------------------------
 -- NO ADJUSTMENTS REQUIRED BELOW THIS LINE
 ------------------------------------------
@@ -83,11 +96,20 @@ local function has_value(tab, val)
     return false
 end
 
+local function zd_shouldRemoveWorldBuff(unit)
+    if Config.NoWorldBuffMaps ~= 1 then
+        return false
+    else
+        local mapId = unit:GetMap():GetMapId()
+        return has_value(Config_NoWorldBuffMaps, mapId)
+    end
+end
+
 local function zd_shouldDebuffRaid(unit)
-    local zone = unit:GetZoneId()
     if Config.RaidActive ~= 1 then
         return false
     else
+        local zone = unit:GetZoneId()
         return has_value(Config_RaidZones, zone)
     end
 end
@@ -153,6 +175,18 @@ local function zd_debuffPetPvP(pet)
     pet:CastCustomSpell(pet, 72341, false, ConfigPvP.DamageTaken,ConfigPvP.DamageDone)
 end
 
+local function zd_removeWorldbuffs(player)
+    for index, value in ipairs(Config_WorldBuff) do
+        player:RemoveAura(tonumber(value))
+    end
+end
+
+local function zd_removeWorldbuffsPet(pet)
+    for index, value in ipairs(Config_WorldBuff) do
+        pet:RemoveAura(tonumber(value))
+    end
+end
+
 local function zd_removeDebuff(player)
     player:RemoveAura(63388)
     player:RemoveAura(72341)
@@ -163,6 +197,9 @@ local function zd_removeDebuffPet(pet)
 end
 
 local function zd_checkPlayerZone(player)
+    if zd_shouldRemoveWorldBuff(player) then
+        zd_removeWorldbuffs(player)
+    end
     if zd_shouldDebuffRaid(player) then
         zd_removeDebuff(player)
         zd_debuffRaid(player)
@@ -178,6 +215,9 @@ local function zd_checkPlayerZone(player)
 end
 
 local function zd_checkPetZone(pet)
+    if zd_shouldRemoveWorldBuff(pet) then
+        zd_removeWorldbuffsPet(pet)
+    end
     if zd_shouldDebuffRaid(pet) then
         zd_removeDebuffPet(pet)
         zd_debuffPetRaid(pet)
@@ -208,7 +248,7 @@ local function zd_checkZoneResurrect(event, player)
     zd_checkPlayerZone(player)
 end
 
-if Config.RaidActive == 1 or Config.DungeonActive == 1 or Config.PvPActive == 1 then
+if Config.RaidActive == 1 or Config.DungeonActive == 1 or Config.PvPActive == 1 or Config.NoWorldBuffMaps == 1 then
     RegisterPlayerEvent(PLAYER_EVENT_ON_LOGIN, zd_checkZoneLogin)
     RegisterPlayerEvent(PLAYER_EVENT_ON_MAP_CHANGE, zd_checkZoneUpdate)
     RegisterPlayerEvent(PLAYER_EVENT_ON_PET_SPAWNED, zd_checkZonePetSpawned)
